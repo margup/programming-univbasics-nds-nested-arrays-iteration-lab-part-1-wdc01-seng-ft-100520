9f82f18da1c8609a3fffae6183d82c23d268170a
bright-lifecycle-9875 <mairgup@icloud.com>
2020-09-17 21:11:12 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ def find_even_values(src)
   count = 0
   while count < src[count].count do 
     inner_count = 0 
-    while inner_count < src[count] do
+    while inner_count < src[count].count do
       if src[count][inner_count].even?
         p src[count][inner_count]
       end
